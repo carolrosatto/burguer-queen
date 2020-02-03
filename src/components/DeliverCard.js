2eaea3a201d7500379b404e6540e275cd3bf5f61
carolrosatto <carolrosatto@gmail.com>
2020-02-02 23:16:23 -0300

    gitignore

---
+++ src/components/DeliverCard.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
+import Button from '../components/Button'
+
+function DeliverCard(props) {
+  return (
+    <div
+      className={css(styles.deliverCard)}>
+      <section className={css(styles.deliverText)}>
+        <p><strong>{props.clientName}, Mesa: {props.tableNumber}</strong></p>
+        <div>{props.clientOrder}</div>
+        {/* <p><strong>Status: {props.status}</strong></p> */}
+        {/* <p>{props.time}</p> */}
+        <div className={css(styles.deliverBtnDiv)}><Button onClick={props.onClick} className={css(styles.deliverBtn)} title="Servir"/></div>
+      </section>
+    </div>
+  )
+}
+const styles = StyleSheet.create({
+  deliverCard: {
+    backgroundColor: '#F29F05',
+    width: '20vw',
+    height: '25vh',
+    fontFamily: 'Lato, sans-serif',
+    fontSize: '100%',
+    display: 'flex',
+    padding: '5px',
+    margin: '25px 3px 3px 25px',
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    overflow: 'scroll'
+  },
+  deliverText: {
+    padding: '5px',
+  },
+  deliverBtn: {
+    width: '10vw',
+    height: '6vh',
+    backgroundColor: '#D95204',
+    borderRadius: '5px',
+    fontFamily: 'Lato, sans-serif',
+    fontSize: '120%',
+    fontWeight: 'bold',
+    border: 'none',
+  },
+  deliverBtnDiv: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '10px'
+  }
+})
+
+export default DeliverCard;
--- src/pages/Deliver.js
+++ src/pages/Deliver.js
@@ -1,18 +1,45 @@
-// import { db } from '../utils/firebase';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { db } from '../utils/firebase';
 import { StyleSheet, css } from 'aphrodite';
-
-// Fazer um setor com os pedidos prontos para entrega (vindo da cozinha)
-
-//Setor com pedidos entregues
+import DeliverCard from '../components/DeliverCard'
 
 function Deliver() {
+  const [finishedOrders, setfinishedOrders] = useState([]);
+  useEffect(() => {
+    db.collection('Order')
+      .orderBy("startTime", "asc")
+      .onSnapshot((snapshot) => {
+        const orders = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }))
+        setfinishedOrders(orders);
+      });
+  }, []);
+
   return (
     <main className={css(styles.mainDeliver)}>
-      <div>
-
+    <div>
+      <div className={css(styles.pendentDeliverDiv)}>
+        <h2 className={css(styles.deliverTitle)}>Pronto para entrega</h2>
+        {finishedOrders
+          .filter(item => item.status === "Finalizado")
+          .map((ord) =>
+            <DeliverCard key={ord.id}
+              clientName={ord.clientName}
+              tableNumber={ord.tableNumber}
+              // time={ord.startTime}
+              // onClick={()=> getFinishTime(ord.id)}
+              status={ord.status}
+              clientOrder={ord.clientOrder.map((item, index) => (
+                <div key={index}>
+                  {item.count}x {item.name}
+                </div>
+              ))}
+            />)}
       </div>
-    </main>
+    </div>
+  </main>
   );
 }
 
@@ -21,8 +48,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#26140A',
     padding: '10px 0 0 0',
     width: '100vw',
-    height: '90.5vh'
+    height: '90.5vh',
   },
+  deliverTitle: {
+    color: '#D9A273',
+    fontFamily: 'Lato, sans-serif',
+    marginLeft: '1vw'
+  }
 })
 
 export default Deliver;
